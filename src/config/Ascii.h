33a6b2e050af8ad820a1b78f9bbf9fab05a273ba
IndieProof <37383570+IndieProof@users.noreply.github.com>
2018-12-07 13:57:08 -0800

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -4,16 +4,6 @@
 
 #pragma once
 
-const std::string windowsAsciiArt =
- "\n                                                                           \n"
-   " ██╗███╗   ██╗██████╗ ██╗███████╗██████╗ ██████╗  ██████╗  ██████╗ ███████╗\n"
-   " ██║████╗  ██║██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔═══██╗██╔════╝\n"
-   " ██║██╔██╗ ██║██║  ██║██║█████╗  ██████╔╝██████╔╝██║   ██║██║   ██║█████╗  \n"
-   " ██║██║╚██╗██║██║  ██║██║██╔══╝  ██╔═══╝ ██╔══██╗██║   ██║██║   ██║██╔══╝  \n"
-   " ██║██║ ╚████║██████╔╝██║███████╗██║     ██║  ██║╚██████╔╝╚██████╔╝██║     \n"
-   " ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝     \n",
-
-const std::string nonWindowsAsciiArt = 
 "\n                                                                           \n"
    " ██╗███╗   ██╗██████╗ ██╗███████╗██████╗ ██████╗  ██████╗  ██████╗ ███████╗\n"
    " ██║████╗  ██║██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔═══██╗██╔════╝\n"
@@ -26,8 +16,4 @@ const std::string nonWindowsAsciiArt =
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
    the art itself, and remove these two #ifdefs and above ascii arts */
-#ifdef _WIN32
-const std::string asciiArt = windowsAsciiArt;
-#else
-const std::string asciiArt = nonWindowsAsciiArt;
-#endif
+
